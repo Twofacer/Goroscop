1b1a5e47cc6b58faa3ff8f3c0583b951416c46ca
Vitaly <viocar112@gmail.com>
2019-03-23 19:16:46 +0200

    Добавил стихии и цвета

--- Goroskop/scritp.js
+++ Goroskop/scritp.js
@@ -1,6 +1,7 @@
 function play()
 {
-    alert(getZoo(document.getElementById("myYear").value));
+    var year = document.getElementById("myYear").value;
+    alert("Символ года: "+getZoo(year)+", стихия: "+getColor(year,1)+", цвет: "+getColor(year,0));
 }
 
 function year_plus(year)
@@ -27,4 +28,27 @@ function getZoo(year){
         default: zoo="некоректное значение года"; break;
     }
  return zoo; 
+}
+
+function getColor(year, info){
+    var color="";
+    var st='';
+    var o_year = Number(year) % 10;
+    switch(o_year)
+    {
+        case 0: color="белый";st="меттал";break;
+        case 1: color="белый";st="меттал";break;
+        case 2: color="черный";st="вода";break;
+        case 3: color="черный";st="вода";break;
+        case 4: color="зеленый";st="дерево";break;
+        case 5: color="зеленый";st="дурево";break;
+        case 6: color="красный";st="огонь";break;
+        case 7: color="красный";st="огонь";break;
+        case 8: color="коричневый";st="земля";break;
+        case 9: color="бкоричневый";st="земля";break;
+    }
+    if (info==0)
+    return color;
+    else
+        return st;
 }
